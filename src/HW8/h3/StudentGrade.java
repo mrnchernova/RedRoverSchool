f85619d790848625f7dfeeddc715316d7405cb5f
mchernova <cherraniram@gmail.com>
2024-09-22 23:36:58 +0300

    hw8 done

---
+++ src/HW8/h3/StudentGrade.java
@@ -0,0 +1,14 @@
+package HW8.h3;
+
+public class StudentGrade {
+    String name;
+    Integer[] grades;
+
+    public void enterGrade(int sessionNumber, int grade){
+        grades[sessionNumber-1] = grade;
+    }
+
+    public Integer getGrade(int sessionNumber){
+       return grades[sessionNumber -1];
+    }
+}
