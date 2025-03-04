90527c9a9efc62c62c18bc173095e5232633caba
mchenova <mchernova3706@gmail.com>
2025-03-04 12:23:51 +0300

    HW9+review

--- src/HW2025/HW9/MonthUtils.java
+++ src/HW2025/HW9/MonthUtils.java
@@ -2,18 +2,25 @@ package HW2025.HW9;
 
 public class MonthUtils {
 
-//        Month[] monthArray = new Month[12];
-//        monthArray[0] = new Month("January", 31, 23);
-//        monthArray[1] = new Month("February", 28, 20);
-//        monthArray[2] = new Month("March", 31, 21);
-//        monthArray[3] = new Month("April", 30, 22);
-//        monthArray[4] = new Month("May", 31, 22);
-//        monthArray[5] = new Month("June", 30, 21);
-//        monthArray[6] = new Month("July", 31, 23);
-//        monthArray[7] = new Month("August", 31, 21);
-//        monthArray[8] = new Month("September", 30, 22);
-//        monthArray[9] = new Month("October", 31, 23);
-//        monthArray[10] = new Month("November", 30, 20);
-//        monthArray[11] = new Month("December", 31, 23);
+
+    public static final Month JAN = new Month("January", 31, 23);
+    public static final Month FEB = new Month("February", 28, 20);
+    public static final Month MAR = new Month("March", 31, 21);
+    public static final Month APR = new Month("April", 30, 22);
+    public static final Month MAY = new Month("May", 31, 22);
+    public static final Month JUN = new Month("June", 30, 21);
+    public static final Month JUL = new Month("July", 31, 23);
+    public static final Month AUG = new Month("August", 31, 21);
+    public static final Month SEP = new Month("September", 30, 22);
+    public static final Month OCT = new Month("October", 31, 23);
+    public static final Month NOV = new Month("November", 30, 20);
+    public static final Month DEC = new Month("December", 31, 23);
+
+
+    public static final Month[] FULL_YEAR = {JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+    public static final Month[] H1 = {JAN, FEB, MAR, APR, MAY, JUN};
+    public static final Month[] Q1 = {APR, MAY, JUN};
+
+
 
 }
